4daec0a1a9a4801ecd8266048563bd69f75b6bed
JLeinon <jleinon@fsc-corp.org>
2026-04-09 13:25:38 -0500

    Update LastReboot.bat

--- LastReboot.bat
+++ LastReboot.bat
@@ -1,1 +1,1 @@
-powershell -ExecutionPolicy Bypass -Command "Write-Output (Get-CimInstance Win32_OperatingSystem).LastBootUpTime"
+powershell -ExecutionPolicy Bypass -Command "(Get-CimInstance Win32_OperatingSystem).LastBootUpTime"
